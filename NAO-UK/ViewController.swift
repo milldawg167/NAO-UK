d9052a1c329db702ec059a254bb8446c5d1175f0
Andrew Miller <milldawg@github.com>
2018-12-20 22:33:53 +0000

    Initial commit

---
+++ NAO-UK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NAO-UK
+//
+//  Created by Andrew Miller on 20/12/2018.
+//  Copyright © 2018 Andrew Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NAO-UKTests/NAO_UKTests.swift
@@ -0,0 +1,34 @@
+//
+//  NAO_UKTests.swift
+//  NAO-UKTests
+//
+//  Created by Andrew Miller on 20/12/2018.
+//  Copyright © 2018 Andrew Miller. All rights reserved.
+//
+
+import XCTest
+@testable import NAO_UK
+
+class NAO_UKTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
